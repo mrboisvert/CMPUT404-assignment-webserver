c64657124e350f9b7abcaa310c7b65ef7cbb18b3
mrboisvert <michaelboisvert6@gmail.com>
2021-09-20 12:46:16 -0600

    implemented server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -32,7 +33,55 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        decoded_data = self.data.decode('utf-8').split('\r\n')
+        status_line = decoded_data[0].split(" ")
+
+        if status_line[0] != "GET":
+            # Only get methods supported, return 405 error for all other methods
+            self.request.sendall(bytearray("HTTP/1.1 405 Method not allowed",'utf-8'))
+            return
+
+        requested_file = "www" + status_line[1]
+        if os.path.isdir(requested_file):
+            if requested_file.endswith("/"):
+                # If the request is a directory send the relative index.html
+                requested_file += "index.html" 
+            else:
+                # Use a 301 code to correct the path
+                self.request.sendall(bytearray("HTTP/1.1 301 Moved Permanently\r\n",'utf-8'))
+                self.request.sendall(bytearray("Location: " + status_line[1] + "/",'utf-8'))
+                return
+
+        if os.path.isfile(requested_file):
+            # Handle the content type
+            content_type_line = "Content-Type: "
+            if requested_file.endswith(".html"):
+                content_type_line += "text/html; charset=utf-8\r\n"
+            elif requested_file.endswith(".css"):
+                content_type_line += "text/css; charset=utf-8\r\n"
+
+            # Handle the content length
+            content_length_line = "Content-Length: " + str(os.path.getsize(requested_file)) + "\r\n"
+
+            # Read the requested file
+            file_in = open(requested_file, "rb")
+            file_data = file_in.read()
+            file_in.close()
+
+            # Send the response header
+            self.request.sendall(bytearray("HTTP/1.1 200 OK\r\n",'utf-8'))
+            self.request.sendall(bytearray(content_type_line,'utf-8'))
+            self.request.sendall(bytearray("Connection: close\r\n",'utf-8'))
+            self.request.sendall(bytearray(content_length_line,'utf-8'))
+
+            # Send the response body
+            self.request.sendall(bytearray("\r\n",'utf-8'))
+            self.request.sendall(file_data)
+        else:
+            # The requested file was not found, so send a 404 error
+            self.request.sendall(bytearray("HTTP/1.1 404 Not found",'utf-8'))
+            return
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
